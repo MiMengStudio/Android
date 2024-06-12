4191b75306e7260dc7ed5df96f672908ac861550
fnstudio <7921406+xunya@user.noreply.gitee.com>
2024-06-12 21:05:45 +0800

    新增开屏

--- app/src/main/java/com/mimeng/LaunchActivity.java
+++ app/src/main/java/com/mimeng/LaunchActivity.java
@@ -29,7 +29,7 @@ public class LaunchActivity extends BaseActivity {
         int uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                 | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
                 | View.SYSTEM_UI_FLAG_FULLSCREEN;
 
         decorView.setSystemUiVisibility(uiOptions);
