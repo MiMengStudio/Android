d13ec7b6750aa3b5cd5ad45c785f765dd36bb609
fnstudio <7921406+xunya@user.noreply.gitee.com>
2024-06-29 21:42:58 +0800

    更新一些颜色配置（并未使用很多）

--- app/src/main/java/com/mimeng/BaseClass/BaseActivity.java
+++ app/src/main/java/com/mimeng/BaseClass/BaseActivity.java
@@ -101,6 +101,7 @@ public class BaseActivity extends AppCompatActivity {
         View decorView = getWindow().getDecorView();
         int uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                 | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                 | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
                 | View.SYSTEM_UI_FLAG_FULLSCREEN;
