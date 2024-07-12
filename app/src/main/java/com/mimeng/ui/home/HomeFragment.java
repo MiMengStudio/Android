8f17d48a209fc8877a1a4da5253121e2e34b440e
TatsukiMengChen <s3224815186@163.com>
2024-07-12 22:45:56 +0800

    初步设计主页

--- app/src/main/java/com/mimeng/ui/home/HomeFragment.java
+++ app/src/main/java/com/mimeng/ui/home/HomeFragment.java
@@ -4,42 +4,54 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.youth.banner.Banner;
+import com.youth.banner.adapter.BannerImageAdapter;
+import com.youth.banner.holder.BannerImageHolder;
+import com.youth.banner.indicator.CircleIndicator;
 import com.mimeng.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
 
-    /**
-     * 建议采用直接引入布局文件即可，不用使用View Model，那样太麻烦了
-     * 不引用ViewModel需要重载几个抽象方法：onCreateView、onViewCreate
-     */
+    private Banner banner;
+    private List<Integer> bannerData;
 
-    // 这个方法用来绑定布局
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        // 这个是原始的，注释掉它，使用LayoutInflater方法
-        // return super.onCreateView(inflater, container, savedInstanceState);
-
-        return LayoutInflater.from(requireActivity()).inflate(R.layout.fragment_home,container,false);
+        return inflater.inflate(R.layout.fragment_home, container, false);
     }
 
-    // 这个方法用来绑定布局中的控件(Id)
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        // 下面我将做个如何使用TextView控件的示例，要找到控件id，必须使用onViewCreated的形参(view)
-        TextView textView = view.findViewById(R.id.text_home);
-        textView.setText("这是首页");
+
+        // 初始化轮播图和数据
+        banner = view.findViewById(R.id.home_banner);
+        initData();
+
+        banner.setAdapter(new BannerImageAdapter<Integer>(bannerData) {
+            @Override
+            public void onBindView(BannerImageHolder holder, Integer data, int position, int size) {
+                holder.imageView.setImageResource(data);
+            }
+        });
+
+        banner.setIndicator(new CircleIndicator(requireContext()));
+        banner.start();
     }
 
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
+    private void initData() {
+        bannerData = new ArrayList<>();
+        bannerData.add(R.drawable.sz);
+        bannerData.add(R.drawable.sz);
+        bannerData.add(R.drawable.sz);
     }
 }
