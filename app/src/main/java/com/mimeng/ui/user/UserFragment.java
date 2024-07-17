f409158e8442365bb5da380a45aa68839c822dfb
TatsukiMengChen <s3224815186@163.com>
2024-07-17 15:57:50 +0800

    初步设计用户界面

--- app/src/main/java/com/mimeng/ui/user/UserFragment.java
+++ app/src/main/java/com/mimeng/ui/user/UserFragment.java
@@ -7,32 +7,24 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.mimeng.R;
 import com.mimeng.databinding.FragmentUserBinding;
 
 public class UserFragment extends Fragment {
 
     private FragmentUserBinding binding;
 
+    @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        UserViewModel userViewModel =
-                new ViewModelProvider(this).get(UserViewModel.class);
-
-        binding = FragmentUserBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-
-        final TextView textView = binding.textUser;
-        userViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_user, container, false);
     }
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
     }
 }
