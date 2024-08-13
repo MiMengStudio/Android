bf46b14ce9906bf8bada542f159f70dafb5074f4
StudioFn <7921406+xunya@user.noreply.gitee.com>
2024-08-13 23:47:38 +0800

    加了一个上拉加载框架，和WebViewActivity一点改动，以及其他的小改动

--- app/src/main/java/com/mimeng/fragments/SearchArticleFragment.java
+++ app/src/main/java/com/mimeng/fragments/SearchArticleFragment.java
@@ -1,6 +1,7 @@
 package com.mimeng.fragments;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -9,12 +10,15 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.gson.reflect.TypeToken;
 import com.mimeng.ApiRequestManager;
 import com.mimeng.App;
+import com.mimeng.R;
+import com.mimeng.activity.WebViewActivity;
 import com.mimeng.adapters.ArticleRecAdapter;
 import com.mimeng.base.BaseFragment;
 import com.mimeng.databinding.FragmentSearchArticleBinding;
@@ -22,8 +26,6 @@ import com.mimeng.values.ArticleEntity;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.logging.Handler;
-import java.util.logging.LogRecord;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -34,7 +36,7 @@ public class SearchArticleFragment extends BaseFragment {
     private final String TAG = "SearchArticleFragment";
     private ArrayList<ArticleEntity> arData = new ArrayList<>();
     private ArticleRecAdapter adapter;
-    
+
     public void search(String keyWord) {
         startSearchArticle(keyWord);
     }
@@ -50,24 +52,36 @@ public class SearchArticleFragment extends BaseFragment {
         manager.setOrientation(LinearLayoutManager.VERTICAL);
         recyclerView.setLayoutManager(manager);
         recyclerView.setAdapter(adapter);
+        recyclerView.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.md_theme_background));
+
+        // 文章点击监听
+        adapter.setItemChangeListener(arId -> {
+            Intent i = new Intent(requireContext(), WebViewActivity.class);
+            i.putExtra("url", "");
+            i.putExtra("showMenu", false);
+            i.putExtra("showCloseBut", false);
+            startActivity(i);
+        });
 
         return binding.getRoot();
     }
 
     public void startSearchArticle(String word) {
-        ApiRequestManager.DEFAULT.searchArticle(word, requireContext(), new Callback() {
+        ApiRequestManager.DEFAULT.searchArticle(word, requireContext(),new Callback() {
             @Override
             public void onFailure(Call call, IOException e) {
 
             }
 
+            @SuppressLint("NotifyDataSetChanged")
             @Override
             public void onResponse(Call call, Response response) {
                 try {
                     assert response.body() != null;
                     String json = response.body().string();
                     Log.d(TAG, "onResponse: 返回的数据=> " + json);
-                    arData = App.GSON.fromJson(json, new TypeToken<>() {});
+                    arData = App.GSON.fromJson(json, new TypeToken<>() {
+                    });
                     requireActivity().runOnUiThread(() -> {
                         adapter.setData(arData);
                         adapter.notifyDataSetChanged();
